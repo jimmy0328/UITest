c8e34bd6b7b637ff941a2f5bd158500385c4315b
lin jimmy <linjimmy@lindeMacBook-Pro.local>
2014-05-16 17:58:55 +0800

    refactor startTest

--- src/com/drsays/testing/unit/startTest.java
+++ src/com/drsays/testing/unit/startTest.java
@@ -1,21 +1,34 @@
 package com.drsays.testing.unit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.drsays.testing.Chrome;
 import com.drsays.testing.Firefox;
 import com.drsays.testing.Safari;
+import com.drsays.testing.TestCase;
 
 public class startTest {
 
+	List<TestCase> cases = new ArrayList<TestCase>();
+
 	public void run() throws InterruptedException {
-		new LoginCase().run(new Chrome());
-		Thread.sleep(5000);
-		new LoginCase().run(new Firefox());
-		Thread.sleep(5000);
-		new FormCase().run(new Chrome());
-		Thread.sleep(5000);
-		new FormCase().run(new Firefox());
-		Thread.sleep(5000);
-		new FormCase().run(new Safari());
+
+		addAllTestCase();
+
+		for (TestCase c : cases) {
+			c.run(new Chrome());
+			Thread.sleep(5000);
+			c.run(new Firefox());
+			Thread.sleep(5000);
+			c.run(new Safari());
+			Thread.sleep(5000);
+		}
+	}
+
+	public void addAllTestCase() throws InterruptedException {
+		cases.add(new LoginCase());
+		cases.add(new FormCase());
 	}
 
 	public static void main(String[] args) {
